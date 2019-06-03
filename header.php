89cc260a275adf78e97568af7b1cdcffdf990b6d
Elias Burhan <amoozgaraf@gmail.com>
2019-06-03 05:02:03 +0430

    added title

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
+<title>Company Main Website</title>
 
 <?php wp_head(); ?>
 <?php astra_head_bottom(); ?>
